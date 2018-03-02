d68b4efa3ac785cb25ddd7d5a4520b60eb284937
williampadfield <35707688+williampadfield@users.noreply.github.com>
2018-03-02 14:31:02 -0600

    updated w/ for loops, etc
    Updated for all 4 news organizations

--- Scraping News Sites.R
+++ Scraping News Sites.R
@@ -42,57 +42,119 @@ for (i in 1:length(urlslist)){
     } ##end for loop
 
 
-#Let's try blurbs on Politics page
-url_nytpoli = 'https://www.nytimes.com/section/politics?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Politics&WT.nav=page'
-webpage_nytpoli = read_html(url_nytpoli)
-blurb_data = html_nodes(webpage,'.summary')
-head(blurb_data) #same here
-
-#dates will be hard, as they are listed like 8hrs ago, 20min ago, etc.
-
 #Bueno, aquí está la página politica de NPR
 url2 = 'https://www.npr.org/sections/politics/'
 webpage2 = read_html(url2)
-headline_data = html_nodes(webpage2,'.title a')
+headline_data2 = html_nodes(webpage2,'.title a')
 #headline_data = html_text(headline_data)
 #head(headline_data) #Very nice. NPR Politics does not show a huge list on its front page
 
 #Los Localizadores Uniformes de Recursos (URLs)
-attr_data2 = html_attrs(headline_data) 
+attr_data2 = html_attrs(headline_data2) 
 attr_data2
 
 urlslist2 = unlist(attr_data2)
 urlslist2 = urlslist2[grep("http", urlslist2)]
 urlslist2
 
-headline_data2 = html_text(webpage,'#ad-mobilebackfill-wrap+ p , .inset2col+ p , p+ p')
-headline_data2 #huh, look at this. We are getting some text, but also much crap
-#Y los blurbs...
-blurb_data = html_nodes(webpage2,'.teaser a')
-blurb_data = html_text(blurb_data)
-head(blurb_data) #some html weirdness here, but I bet we could handle it
+##start a data frame
+NPRDF = matrix(NA, nrow = length(urlslist2), ncol = 3)
+colnames(NPRDF) = c("Source", "Url", "Text")
+NPRDF = as.data.frame(NPRDF)
+
+##for loops
+for (i in 1:length(urlslist2)){
+  
+  ##read in the URL
+  webpage2 <- read_html(urlslist2[i])
+  
+  ##pull the specific nodes
+  headline_data2 = html_nodes(webpage2,'#storytext > p') 
+  
+  ##pull the text
+  text_data2 = html_text(headline_data2)
+  
+  ##save the data
+  NPR$Source[i] = "NPR"
+  NPRDF$Url[i] = urlslist2[i]
+  NPRDF$Text[i] = paste(text_data2, collapse = "")
+} ##end for loop
 
 #Y ahora, el Fox News
 url3 = 'http://www.foxnews.com/politics.html'
 webpage3 = read_html(url3)
-headline_data = html_nodes(webpage3, '.story- a , .article-list .title a')
-headline_data = html_text(headline_data)
-head(headline_data) #doesn't look bad, the output that is...
+headline_data3 = html_nodes(webpage3, '.story- a , .article-list .title a')
+#headline_data = html_text(headline_data)
+head(headline_data3) #doesn't look bad, the output that is...
 
-#Fox Blurbs Channel - 'We Blurb, You Decide'
-blurb_data = html_nodes(webpage3,'.dek a')
-blurb_data = html_text(blurb_data)
-head(blurb_data) #How lovely, it works!
+attr_data3 = html_attrs(headline_data) 
+attr_data3
+
+urlslist3 = unlist(attr_data3)
+urlslist3 = urlslist3[grep("http", urlslist3)]
+urlslist3 = unique(urlslist3)
+urlslist3
+
+##start a data frame
+FoxDF = matrix(NA, nrow = length(urlslist3), ncol = 3)
+colnames(FoxDF) = c("Source", "Url", "Text")
+FoxDF = as.data.frame(FoxDF)
+
+##for loops
+for (i in 1:length(urlslist3)){
+  
+  ##read in the URL
+  webpage3 <- read_html(urlslist3[i])
+  
+  ##pull the specific nodes
+  headline_data3 = html_nodes(webpage3,'p+ p , .fn-video+ p , .twitter+ p , .speakable') 
+  
+  ##pull the text
+  text_data3 = html_text(headline_data3)
+  
+  ##save the data
+  FoxDF$Source[i] = "Fox News"
+  FoxDF$Url[i] = urlslist3[i]
+  FoxDF$Text[i] = paste(text_data3, collapse = "")
+} ##end for loop
 
 #Oh God, it's time to do Breitbart :(
-url4 = 'http://www.breitbart.com/'
+url4 = 'http://www.breitbart.com/big-government/'
 webpage4 = read_html(url4)
-headline_data = html_nodes(webpage4, '.title a , #grid-block-0 span , #BBTrendUL a , #disqus-popularUL a , font')
-headline_data = html_text(headline_data)
-head(headline_data) #Their headlines often show up as disjointed sentence fragments, but that's how they really are
+headline_data4 = html_nodes(webpage4, '.title a , #grid-block-0 span , #BBTrendUL a , #disqus-popularUL a , font')
+headline_data4 = html_text(headline_data4)
+head(headline_data4) 
 
-#Breitbart apparently does not do blurbs on its homepage - probably too much nuance...
+attr_data4 = html_attrs(headline_data4) 
+attr_data4
 
+urlslist4 = unlist(attr_data4)
+urlslist4 = urlslist4[grep("http", urlslist4)]
+urlslist4 = unique(urlslist4)
+urlslist4
+
+##start a data frame
+BreitbartDF = matrix(NA, nrow = length(urlslist4), ncol = 3)
+colnames(BreitbartDF) = c("Source", "Url", "Text")
+BreitbartDF = as.data.frame(BreitbartDF)
+
+##for loops
+for (i in 1:length(urlslist4)){
+  
+  ##read in the URL
+  webpage <- read_html(urlslist4[i])
+  
+  ##pull the specific nodes
+  headline_data4 = html_nodes(webpage4,'.entry-content p , h2') 
+  
+  ##pull the text
+  text_data4 = html_text(headline_data4)
+  
+  ##save the data
+  BreitbartDF$Source[i] = "Breitbart"
+  BreitbartDF$Url[i] = urlslist4[i]
+  BreitbartDF$Text[i] = paste(text_data4, collapse = "")
+} ##end for loop
 ##fix the above so they all look like NYtimesDF (with different names)
 
 ##set your working directory
@@ -102,7 +164,7 @@ head(headline_data) #Their headlines often show up as disjointed sentence fragme
 overalldata = read.csv("overalldata.csv")
 
 ##combine with new data (add the other DF names in here...)
-newdata = rbind(overalldata, NYtimesDF)
+newdata = rbind(overalldata, NYtimesDF, NPRDF, FoxDF, BreitbartDF)
 
 ##make the newdata unique in case of overlap across days
 newdata = unique(newdata)
